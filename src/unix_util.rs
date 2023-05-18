4ba97462c056f1bacf9235b9d82201f4f555bc44
Solra Bizna <solra@bizna.name>
2023-05-17 21:59:10 -0600

    Remove dependency on nix (it's silly to pull in the entire crate just for a single system call)

--- src/unix_util.rs
+++ src/unix_util.rs
@@ -1,8 +1,6 @@
 //! This module contains utility functions required for proper functioning on
 //! UNIX.
 
-use nix::sys::signal::{raise, Signal};
-
 pub(crate) fn sigstop_ourselves() {
-    let _ = raise(Signal::SIGSTOP);
+    unsafe { libc::raise(libc::SIGSTOP); }
 }
